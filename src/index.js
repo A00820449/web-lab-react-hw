bb2e596bda49ba873e0c81ca67b071fc5105bca5
Miguel Tornero <a00820449@itesm.mx>
2022-09-14 13:19:31 -0500

    added tie status

--- src/index.js
+++ src/index.js
@@ -118,7 +118,11 @@ class Game extends React.Component {
         let status
         if (winner) {
             status = `Winner ${winner}`
-        } else {
+        } 
+        else if (this.state.stepNumber >= 9) {
+            status = `Tie`
+        }
+        else {
             status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
         }
         return (
