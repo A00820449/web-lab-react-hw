9efbf709d847ab820f591e95f38138e92aafae7f
Miguel Tornero <a00820449@itesm.mx>
2022-09-14 15:01:15 -0500

    fixed nevbar links

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 
 export default function Menu() {
+    const homepageURL = process.env.PUBLIC_URL || "/"
     return (
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a href={"/"} className={"navbar-brand"}>Mi Proyecto</a>
+            <a href={homepageURL} className={"navbar-brand"}>Mi Proyecto</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a href="/" className="nav-link">Home </a>
+                        <a href={homepageURL} className="nav-link">Home </a>
                     </li>
                     <li className="nav-item">
-                        <a href="/nosotros" className="nav-link">Nosotros </a>
+                        <a href={homepageURL + "nosotros"} className="nav-link">Nosotros </a>
                     </li>
                     <li className="nav-item">
-                        <a href="/servicios" className="nav-link">Servicios </a>
+                        <a href={homepageURL + "servicios"} className="nav-link">Servicios </a>
                     </li>
 
                     <li className="nav-item">
-                        <a href="/contacto" className="nav-link">Contacto </a>
+                        <a href={homepageURL + "contacto"} className="nav-link">Contacto </a>
                     </li>
                 </ul>
             </div>
